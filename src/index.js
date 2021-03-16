5bd5b3aeaabf728edca81e28886c5b5d39efa0bb
Ramunas <dev.nognys@gmail.com>
2021-03-16 10:24:51 +0200

    updated build for vercel

--- src/index.js
+++ src/index.js
@@ -1,28 +1,3 @@
 import "./styles/index.scss";
 
-const greet = "Hello World";
-const newTalk = "New Talks";
-
-const elvenShieldRecipe = {
-  leatherStrips: 5,
-  ironIngot: 1,
-  refinedMoonstone: 6,
-};
-
-const elevenGauntletsRecipe = {
-  ...elvenShieldRecipe,
-  irnIgot: 1,
-  refinedMoonstone: 4,
-};
-
-// const elevenGauntletsRecipe = {
-//   ...elvenShieldRecipe,
-//   leather: 1,
-//   refinedMoonstone: 4,
-// };
-
-console.log(elvenShieldRecipe);
-console.log(elevenGauntletsRecipe);
-
-console.log(greet);
-
+console.log("Hello Media");
