34a6aa8ef1fa1fc3b6f2c76ab61b6d5c9739eebe
Ramunas <dev.nognys@gmail.com>
2021-03-17 06:51:46 +0200

    added font-face support to wbpack, added fonts

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,8 @@ module.exports = {
     // output path is required for `clean-webpack-plugin`
     path: path.resolve(__dirname, "dist"),
     // this places all images processed in an image folder
-    assetModuleFilename: "images/[hash][ext][query]",
+    // assetModuleFilename: "(images)/[hash][ext][query]",
+    // assetModuleFilename: "fonts/[hash][ext][query]",
   },
 
   module: {
@@ -47,26 +48,17 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        /**
-         * The `type` setting replaces the need for "url-loader"
-         * and "file-loader" in Webpack 5.
-         *
-         * setting `type` to "asset" will automatically pick between
-         * outputing images to a file, or inlining them in the bundle as base64
-         * with a default max inline size of 8kb
-         */
-        // type: "asset/inline", good for small images or icons in header
-        type: "asset",
-
-        /**
-         * If you want to inline larger images, you can set
-         * a custom `maxSize` for inline like so:
-         */
-        // parser: {
-        //   dataUrlCondition: {
-        //     maxSize: 30 * 1024,
-        //   },
-        // },
+        type: "asset/resource",
+        generator: {
+          filename: "images/[hash][ext]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[hash][ext]",
+        },
       },
       {
         test: /\.js$/,
@@ -75,19 +67,12 @@ module.exports = {
           // without additional settings, this will reference .babelrc
           loader: "babel-loader",
           options: {
-            /**
-             * From the docs: When set, the given directory will be used
-             * to cache the results of the loader. Future webpack builds
-             * will attempt to read from the cache to avoid needing to run
-             * the potentially expensive Babel recompilation process on each run.
-             */
-            // cacheDirectory: true,
+            cacheDirectory: true,
           },
         },
       },
     ],
   },
-
 
   plugins: plugins,
 
